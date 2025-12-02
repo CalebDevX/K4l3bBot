cec9eefda2ea1a1ee83e316c774a6e11294bf86d
K4l3b <143396031+CalebDevX@users.noreply.github.com>
2025-12-02 21:49:24 +0100

    Update Giphy API key and update URL

--- settings.js
+++ settings.js
@@ -1,16 +1,16 @@
 const settings = {
-  packname: 'Knight Bot',
+  packname: 'K4l3b Bot',
   author: '‎',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
-  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  botName: "K4l3b Bot",
+  botOwner: 'Caleb, // Your name
+  ownerNumber: '2348096567473', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: 'bdETAFBTGlXqIQqogV6EkXkivUDeSlHP',
   commandMode: "public",
   maxStoreMessages: 20, 
   storeWriteInterval: 10000,
   description: "This is a bot for managing group commands and automating tasks.",
   version: "3.0.5",
-  updateZipUrl: "https://github.com/mruniquehacker/Knightbot-MD/archive/refs/heads/main.zip",
+  updateZipUrl: "https://github.com/CalebDevX/K4l3bBot/archive/refs/heads/main.zip",
 };
 
 module.exports = settings;
